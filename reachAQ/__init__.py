29e2450abed16f8e408c9b03c527a6fd0f95a863
Ben Reynolds <Benjamin.g.reynolds@ucdenver.edu>
2026-06-29 18:07:05 -0600

    Add reachAQ wheel entry point

---
+++ reachAQ/__init__.py
@@ -0,0 +1,1 @@
+"""reachAQ application package."""
---
+++ reachAQ/app.py
@@ -0,0 +1,9 @@
+"""Command module for launching the reachAQ acquisition UI."""
+
+import sys
+
+from tools.acquisition.gui import main
+
+
+if __name__ == "__main__":
+    sys.exit(main())
